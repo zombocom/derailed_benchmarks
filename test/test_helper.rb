6bac57ba9c60146837e5ea28ca7ede9ce16104bd
schneems <richard.schneeman+foo@gmail.com>
2019-06-10 22:00:50 -0500

    Update gem file tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -27,11 +27,16 @@ Capybara.default_driver   = :rack_test
 Capybara.default_selector = :css
 
 # Run any available migration
-ActiveRecord::Migrator.migrate File.expand_path("../rails_app/db/migrate/", __FILE__)
+if Rails.gem_version >= Gem::Version.new('5.2.0')
+  ActiveRecord::MigrationContext
+    .new(File.expand_path("../rails_app/db/migrate/", __FILE__))
+    .migrate
+else
+  ActiveRecord::Migrator.migrate(File.expand_path("../rails_app/db/migrate/", __FILE__))
+end
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
 
 class ActiveSupport::IntegrationCase
   def assert_has_content?(content)
